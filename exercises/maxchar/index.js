7169e94748f0cffcb68331186f49719db42e3bc9
killeraliens <killeraliens@outlook.com>
2020-07-03 14:57:27 -0700

    longer solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -8,14 +8,21 @@
 function maxChar(str) {
   const dict = {}
   for (let char of str.split('')) {
-    debugger
-    dict[char]
-      ? dict[char]++
-      : dict[char] = 1
+    dict[char] ? dict[char]++ : dict[char] = 1
   }
-  return Object.entries(dict).sort((a, b) => {
-    return a[1] < b[1] ? 1 : -1
-  })[0][0]
+
+  let maxCount = 0
+  let maxKey = ''
+  for (let [key, val] of Object.entries(dict)) {
+    if (val > maxCount) {
+      maxCount = val
+      maxKey = key
+    }
+  return maxKey
+  }
+  // return Object.entries(dict).sort((a, b) => {
+  //   return a[1] > b[1] ? -1 : 1
+  // })[0][0]
 
 }
 maxChar('abcddd11')
