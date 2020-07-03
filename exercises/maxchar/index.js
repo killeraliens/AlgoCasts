a7e1c19a826cc7b18c0881b1ef99b1c0a484f635
killeraliens <killeraliens@outlook.com>
2020-07-03 14:43:31 -0700

    max char solution 1

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const dict = {}
+  for (let char of str.split('')) {
+    debugger
+    dict[char]
+      ? dict[char]++
+      : dict[char] = 1
+  }
+  return Object.entries(dict).sort((a, b) => {
+    return a[1] < b[1] ? 1 : -1
+  })[0][0]
 
+}
+maxChar('abcddd11')
 module.exports = maxChar;
