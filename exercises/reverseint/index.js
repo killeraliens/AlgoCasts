aed508be4c9e74beb0f1d9415450e6909bb01f8a
killeraliens <killeraliens@outlook.com>
2020-07-02 20:32:09 -0700

    reverse int with math.sign

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// function reverseInt(n) {
+//   if (n < 0) {
+//     const revNeg = '-' + n.toString().split('').filter(char => char !== '-').reverse().join('')
+//     return parseInt(revNeg)
+//   }
+//   return parseInt(n.toString().split('').reverse().join(''))
+// }
+
+function reverseInt(n) {
+  const rev = n.toString().split('').reverse().join('')
+  return parseInt(rev) * Math.sign(n)
+}
 
 module.exports = reverseInt;
