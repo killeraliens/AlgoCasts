73a97b956515c239c77576f2eb77bab3961e13b4
killeraliens <killeraliens@outlook.com>
2020-07-03 18:06:01 -0700

    chunked

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,15 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const result = []
+  const times = Math.ceil(array.length / size)
+  for (let i = 0; i < times; i++ ) {
+    let newArr = array.slice(i * size, i * size + size)
+    result.push(newArr)
+  }
+  return result
+}
 
+chunk([1, 2, 3, 4, 5], 2)
 module.exports = chunk;
